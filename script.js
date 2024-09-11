4f0ddea583ebca1ec1a35d3f727865f2528106ab
Yogesh panchal <93254488+Yogesh82001@users.noreply.github.com>
2024-09-11 18:19:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+$(window).on( 'load', function() {
+	$(".loadding-page").delay(6000).fadeOut(200);
+	$(".cssload-box-loading").on('click', function() {
+	    $(".cssload-box-loading").fadeOut(200);
+	})
+})
